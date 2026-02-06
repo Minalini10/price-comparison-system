80be81237f7ea1358653b12aeb05627d5a8886af
Sri Minalini <sriminalini@gmail.com>
2026-02-06 18:17:36 +0530

    frontend

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+function comparePrices() {
+  const resultsDiv = document.getElementById("results");
+
+  resultsDiv.innerHTML = `
+    <p>Amazon: ₹45,999</p>
+    <p>Flipkart: ₹44,499 <strong>(Best Deal)</strong></p>
+    <p>Myntra: ₹46,200</p>
+  `;
+}
